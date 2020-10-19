bc0904972c8f38d69847955844d92c757383b4db
cchoughton <charliehoughton7@gmail.com>
2020-10-18 21:25:36 -0400

    Add users migration

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: python manage.py migrate users
 release: python manage.py migrate
 web: gunicorn tinyfunds.wsgi
--- tinyfunds/settings.py
+++ tinyfunds/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'tinyfunds',
+    'tinyfunds.users',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -134,6 +135,11 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
 SITE_ID=1
 LOGIN_REDIRECT_URL = '/'
 
@@ -148,3 +154,5 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+AUTH_USER_MODEL = 'users.User'
