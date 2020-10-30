a65305ac6855d7f3cd73752e0219ff78f5c0bf19
Matt Walsh <mattw6500@gmail.com>
2020-10-30 16:27:06 -0400

    Unifished extension of event model

--- tinyfunds/views.py
+++ tinyfunds/views.py
@@ -16,8 +16,40 @@ class HomeView(generic.ListView):
 
     def get_queryset(self):
         return User.objects.all()
-    #def get_queryset(self):
-    #    return Event.objects.filter(
-    #        pub_date__lte=timezone.now()
-    #    ).order_by('-pub_date')[:5]
 
+class ExploreView(generic.ListView):
+    template_name = 'tinyfunds/explore.html'
+    content_object_name = 'event_list' 
+
+    def get_queryset(self):
+        return Event.objects.all()
+
+class EventView(generic.DetailView):
+    template_name = 'account/event.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Event.objects
+
+def event(request, pk):
+    event = get_object_or_404(Event, idnum=pk)
+    if (request.method == "POST"):
+        new_title = request.POST['title'].strip()
+        new_org_name = request.POST['org_name'].strip()
+        new_event_pic = request.POST['event_pic'].strip()
+        new_description = request.POST['description'].strip()
+        new_event_date = request.POST['event_date'].strip()
+        if new_title != "":
+            event.title = new_title
+        if new_description != "":
+            event.description = new_description
+        if new_org_name != "":
+            event.org_name = new_org_name
+        if new_event_pic != "":
+            event.event_pic = new_event_pic
+        event.save()
+    return render(request, 'tinyfunds/explore.html', {
+        'event': event,
+    })
